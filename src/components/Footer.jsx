565d213b122af99b346f0f05cd667ef3e375d2be
JeremiaSihombing <jeremiajordan2001@gmail.com>
2023-06-05 11:44:10 +0700

    FOOTER

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer =()=>{
+    return (
+      <>
+        <div className="bg-[#252641]">
+            <div className="text-white text-center font-bold">Exless</div>
+            <div className="flex text-white text-center ">
+                <span>Careers</span>
+                <span>Privacy Policy</span>
+                <span>Term and Condition</span>
+            </div>
+            
+        </div>
+      </>
+    );
+}
+export default Footer;
