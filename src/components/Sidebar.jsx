52d15b0d7bb1acdb635daa98c5c13cc1009389f3
JeremiaSihombing <jeremiajordan2001@gmail.com>
2023-06-14 21:20:56 +0700

    update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,90 @@
+import React from 'react'
+
 const Sidebar = () => {
-    return (
-        <div>
-            TES
+  return (
+    <div className="flex">
+      <div className="flex flex-col h-screen p-3 bg-white shadow w-60">
+        <div className="space-y-3">
+          <div className="flex items-center">
+            <h2 className="text-xl font-bold">ADMIN AREA</h2>
+          </div>
+          <div className="flex-1">
+            <ul className="pt-2 pb-4 space-y-1 text-sm">
+              <li className="rounded-sm">
+                <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                  </svg>
+                  <span>Dashboard</span>
+                </a>
+              </li>
+              <li className="rounded-sm">
+                <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                    />
+                  </svg>
+                  <span>Account</span>
+                </a>
+              </li>
+              <li className="rounded-sm">
+                <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                    />
+                  </svg>
+                  <span>Community</span>
+                </a>
+              </li>
+              <li className="rounded-sm">
+                <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                    />
+                  </svg>
+                  <span>Video</span>
+                </a>
+              </li>
+              <li className="rounded-sm">
+                <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+                    />
+                  </svg>
+                  <span>Article</span>
+                </a>
+              </li>
+              
+              
+              <li className="rounded-sm">
+                <a href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                  </svg>
+                  <span>Log out</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+      <div className="container mx-auto mt-12">
+       
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar
