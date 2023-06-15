cd60ac56885b6a41b9246893762194be86963fa6
Irsyad Nafi <121683533+IrsyadNafiA@users.noreply.github.com>
2023-06-15 11:19:20 +0700

    update sidebar icon & add daisy ui

--- src/utilities/FormInput.jsx
+++ src/utilities/FormInput.jsx
@@ -2,7 +2,7 @@ const FormInput = (props) => {
     return (
         <div className="w-[370px] flex flex-col gap-1">
             <label htmlFor={props.id}>{props.title}</label>
-            <input className={`text-gray-800 h-[40px] w-[370px] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#f48c06]`} type={props.type} id={props.id} placeholder={props.title} />
+            <input className={`text-gray-800 h-[40px] w-[370px] bg-white p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#f48c06]`} type={props.type} id={props.id} placeholder={props.title} />
         </div>
     )
 }
